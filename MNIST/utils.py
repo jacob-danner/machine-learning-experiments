1cd015b25be34c6c48c72c55f3e2a17299add7c7
Jacob Danner <jacob@jacobdanner.dev>
2024-03-30 23:15:55 +0000

    train conv net classifier to 98.64% accuracy

--- MNIST/utils.py
+++ MNIST/utils.py
@@ -1,6 +1,8 @@
 from torch import tensor, Tensor
 from torchvision import transforms
 from PIL import Image
+from torch_lr_finder import LRFinder
+import numpy as np
 
 
 def display_tensor_as_image(tensor: Tensor):
@@ -22,8 +24,18 @@ class FlattenImage():
     def __call__(self, x):
         return x.view(28*28)
 
+def set_optimal_learning_rate(model, optimizer, loss_fn, data_loader, device, end_lr, num_iter) -> None:
+    '''
+    Using torch_lr_finder.LRFinder, find, plot, and set the optimal learning rate.
+    Updates the optimizer in place.
+    '''
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    FlattenImage()
-])
+    lr_finder = LRFinder(model, optimizer, loss_fn, device=device)
+    lr_finder.range_test(data_loader, end_lr=end_lr, num_iter=num_iter)
+    min_grad_idx = (np.gradient(np.array(lr_finder.history['loss']))).argmin()
+    optimal_lr = lr_finder.history['lr'][min_grad_idx]
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = optimal_lr
+
+    lr_finder.plot()
